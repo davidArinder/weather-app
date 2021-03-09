c7841bdddcf189f550736cce26c2f96b6c5ed186
David Arinder <david.arinder@gmail.com>
2021-03-09 18:47:10 -0500

    added Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => (
+  <div>
+    <h1 className={styles.header}>Your Weather</h1>
+  </div>
+);
+export default Header;
