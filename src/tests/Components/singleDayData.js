6bb076643282ec347c3eafd4be9c95a7a69099de
David Arinder <david.arinder@gmail.com>
2021-03-09 18:59:39 -0500

    added test data for single day test

---
+++ src/tests/Components/singleDayData.js
@@ -0,0 +1,12 @@
+import weatherIcon from "../../weathericon.png";
+
+const singleDayData = {
+  weatherDetails: {
+    temperature: 50,
+    description: "cold",
+    icon: weatherIcon,
+    location: "Detroit",
+  },
+};
+
+export default singleDayData;
