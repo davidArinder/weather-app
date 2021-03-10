05ce7598558d99f2ec7be9e697af56ac9c7a6fc8
David Arinder <david.arinder@gmail.com>
2021-03-09 19:05:24 -0500

    added five day test data and fixed capitalization

---
+++ src/tests/Components/FiveDayData.js
@@ -0,0 +1,44 @@
+import weatherIcon from "../../weathericon.png";
+
+const fiveDayData = {
+  weatherDetails: [
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 19 1975 23:15:30 GMT+0200 (EST)",
+    },
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 19 1975 23:16:30 GMT+0200 (EST)",
+    },
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 19 1975 23:17:30 GMT+0200 (EST)",
+    },
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 20 1975 23:15:30 GMT+0200 (EST)",
+    },
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 20 1975 23:16:30 GMT+0200 (EST)",
+    },
+    {
+      temperature: 45,
+      description: "warmer",
+      icon: weatherIcon,
+      timestamp: "Tue Aug 20 1975 23:17:30 GMT+0200 (EST)",
+    },
+  ],
+};
+
+export default fiveDayData;
